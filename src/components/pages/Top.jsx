70c75075ea17cb4e275b09b2513d1169c0b51725
talkiedoorkey <89324532+talkiedoorkey@users.noreply.github.com>
2022-01-30 20:04:59 +0900

    Making onclick

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,17 @@
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 export const Top = () => {
+  const onClickAdmin = () => alert("管理");
+  const onClickGeneral = () => alert("一般");
+
   return (
     <Scontainer>
       <h2>TOPページです</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </Scontainer>
   );
 };
