1318b493a22cf08b8c8047ba3b67786dfc93157a
talkiedoorkey <89324532+talkiedoorkey@users.noreply.github.com>
2022-02-05 10:21:40 +0900

    Finish Lecture 40

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
-import React, { useContext } from "react";
+import React, { useContext, memo } from "react";
 import { UserContext } from "../../../providers/UserProvider";
 
-export const UserIconWithName = (props) => {
+export const UserIconWithName = memo((props) => {
+  console.log("UserIconWithName");
+
   const { image, name } = props;
   const { userInfo } = useContext(UserContext);
   const isAdmin = userInfo ? userInfo.isAdmin : false;
@@ -14,7 +16,7 @@ export const UserIconWithName = (props) => {
       {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
-};
+});
 
 const SContainer = styled.div`
   text-align: center;
